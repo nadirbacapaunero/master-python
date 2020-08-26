e4976e99ea6a85db6d468fb4d06f51ed98135707
nadir <66783157+nadirbacapaunero@users.noreply.github.com>
2020-08-26 20:32:47 -0300

    Add files via upload
    crear un programa que agregue elementos hasta que la longitud sea de 120, usar For y While

---
+++ master-python/practica/practica6.py
@@ -0,0 +1,24 @@
+"""
+Ejercicio un programa que añada valores a una lista mientras que su longitud sea < a 120
+Mostrar la lista
+con while y for
+"""
+"""
+coleccion = []
+
+for contador in range (0, 120):
+    coleccion.append(f" elemento -  {contador}")
+    print("Mostrar el:" + coleccion[contador])
+
+
+print(coleccion)
+
+"""
+coleccion = []
+x = 0
+while x < 120 :
+    coleccion.append(f"elemento- {x}")
+    print("Mostrando el " + coleccion[x])
+    x += 1
+
+print(coleccion)   
